224541e4fbd5aafcfb01a7ad9ebeff2e034d30a6
cris <civargas5@uc.cl>
2021-02-07 12:55:39 -0300

    mapa chile listo

--- src/app/echarts/line/basic-line-echarts/basic-line-echarts.component.ts
+++ src/app/echarts/line/basic-line-echarts/basic-line-echarts.component.ts
@@ -30,33 +30,24 @@ export class BasicLineEchartsComponent implements OnInit {
 
   private _initBasicLineEchart(chartData : BasicEchartLineModel[]){
 
-    $.get("https://raw.githubusercontent.com/crvargasu/angular---echarts/map/src/assets/echart/all.json", function(usjson){
-      echarts.registerMap('USA', usjson, {
+    $.get("https://raw.githubusercontent.com/crvargasu/angular---echarts/map/src/assets/echart/all.json", function(cljson){
+      echarts.registerMap('Cl', cljson, {
         
       });
     });
     
 
     this._chartOption = {
+      title: {
+        text: 'Chile',
+        left: 'right'
+    },
+
       tooltip: {
         trigger: 'item',
         showDelay: 0,
         transitionDuration: 0.2,
       },
-      visualMap: [
-        {
-          min: 0,
-          max: 7000,
-          dimension: 3,
-          inRange: {
-            color: ['blue', '#121122', 'red'], 
-            symbolSize: [30, 100]
-          },
-          outOfRange: {
-            symbolSize: [30, 100]
-          }
-        }
-      ],
       toolbox: {
         show: true,
         left: 'left',
@@ -69,8 +60,9 @@ export class BasicLineEchartsComponent implements OnInit {
       },
       series: [
         {
+          name: 'Chile Region',
           type: 'map',
-          map: 'USA',
+          map: 'Cl',
           roam: true,
           emphasis: {
             label: {
@@ -78,15 +70,42 @@ export class BasicLineEchartsComponent implements OnInit {
             }
         },
         data:[
-          {name:"Región de Valparaíso", value: 4822023},
-          
-      ]
+          {name:"Región de Aysén del Gral.Ibañez del Campo", value: 104843},
+          {name:"Región de Magallanes y Antártica Chilena", value: 158657},
+          {name:"Región de Arica y Parinacota", value: 183712},
+          {name:"Región de Atacama", value: 280543},
+          {name:"Región de Los Ríos", value: 379709},
+          {name:"Región de Antofagasta", value: 575268},
+          {name:"Región de Tarapacá", value: 683203},
+          {name:"Región de Coquimbo", value: 718717},
+          {name:"Región de Los Lagos", value: 836256},
+          {name:"Región del Libertador Bernardo O'Higgins", value: 883368},
+          {name:"Región de La Araucanía", value: 970419},
+          {name:"Región del Maule", value: 1007831},
+          {name:"Región de Valparaíso", value: 1759167},
+          {name:"Región del Bío-Bío", value: 2036443},
+          {name:"Región Metropolitana de Santiago", value: 6883563},
+      ],
+      
         // data: chartData.map(m => ({
         //   value: m.value,
         //   name: m.name
         // })),
       
-      }]
+      }],
+      visualMap: [
+        {  
+          left: 'right',
+          min: 0,
+          max: 15,
+          dimension: 3,
+          inRange: {
+            color: ['#313695', '#4575b4', '#74add1', '#abd9e9', '#e0f3f8', '#ffffbf', '#fee090', '#fdae61', '#f46d43', '#d73027', '#a50026'], 
+            symbolSize: [30, 100]
+          },
+          text: ['High', 'Low'],
+        }
+      ],
 
     };
 
